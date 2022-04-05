e022a4529560a4edae9c62372cf7c3b44d9d0845
lebiosphere <lebiosphere@gmail.com>
2022-04-05 23:37:50 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,11 @@ radio.setGroup(42)
 basic.forever(function () {
     if (grove.measureInCentimetersV2(DigitalPin.P0) > 5) {
         pins.digitalWritePin(DigitalPin.P1, 1)
+        soundExpression.slide.playUntilDone()
     } else {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    if (input.temperature() < 18 || input.temperature() > 25) {
+    if (input.temperature() < 18 || input.temperature() > 25 || input.buttonIsPressed(Button.A)) {
         basic.showIcon(IconNames.Skull)
         radio.sendString("alerte_t")
         soundExpression.giggle.playUntilDone()
